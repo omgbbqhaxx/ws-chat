262eb6f4b06a0ca28cbbba8ef31093ee9a6ef91a
Yasin Aktimur <chardone@windowslive.com>
2014-11-18 15:38:25 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -6,29 +6,7 @@ import tornado.websocket
 class IndexHandler(tornado.web.RequestHandler):
 
     def get(self):
-        self.write("""
-        <form action="#">
-            <input type="text" />
-            <button type="submit">Echo</button>
-        </form>
-        <br>
-        <ul></ul>
-        <script>
-        var ws = new WebSocket("ws://chatsocial.me:8000/echo");
-        ws.onmessage = function(msg) {
-            var li = document.createElement("li")
-            li.innerHTML = msg.data;
-            document.querySelector("ul").appendChild(li);
-        }
-
-        document.querySelector("form").onsubmit = function () {
-            var i = document.querySelector("input");
-            ws.send(i.value);
-            i.value = "";
-            return false;
-        }
-        </script>
-        """)
+        self.render("index.html")
 
 
 class WSHandler(tornado.websocket.WebSocketHandler):
